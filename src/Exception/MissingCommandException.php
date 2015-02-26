88ee7de24ba139bddf17b2bcbf1b49d7b1127db4
Nigel Greenway <nigel_greenway@me.com>
2015-02-26 19:50:26 +0000

    Adds MissingCommandException back into project

---
+++ src/Exception/MissingCommandException.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace League\Tactician\Exception;
+
+use League\Tactician\Command;
+
+/**
+ * Command has not been added to the mapping array.
+ */
+class MissingCommandException extends \OutOfBoundsException implements Exception
+{
+    /**
+     * @var Command
+     */
+    private $command;
+
+    /**
+     * @param Command $command
+     * @return static
+     */
+    public static function forCommand(Command $command)
+    {
+        $exception = new static('The command ' . get_class($command) . ' has not been addeded to the mapping configuration.');
+        $exception->command = $command;
+
+        return $exception;
+    }
+
+    /**
+     * @return Command
+     */
+    public function getCommand()
+    {
+        return $this->command;
+    }
+}
